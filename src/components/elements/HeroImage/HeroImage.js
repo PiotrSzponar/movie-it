1ae468229626d0b277aa3a5f98afd88263292505
Piotr Szponar <18piotrek06@gmail.com>
2019-04-12 15:05:59 +0200

    Connect with API and add Hero Image

--- src/components/elements/HeroImage/HeroImage.js
+++ src/components/elements/HeroImage/HeroImage.js
@@ -3,8 +3,23 @@ import './HeroImage.css';
 
 const HeroImage = (props) => {
     return (
-        <div>
-            HeroImage
+        <div className="mi-heroimage"
+            style={{
+                background:
+                    `linear-gradient(
+                        to bottom,
+                        rgba(0, 0, 0, 0) 30%,
+                        rgba(0, 0, 0, 0.7) 70%),
+                    url('${props.image}'),
+                    #1c1c1c`
+            }}
+        >
+            <div className="mi-heroimage-content">
+                <div className="mi-heroimage-text">
+                    <h1>{props.title}</h1>
+                    <p>{props.text}</p>
+                </div>
+            </div>
         </div>
     )
 }
